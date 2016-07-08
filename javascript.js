4b65bba6afa29c0635672fc0ad98cd4e9ff67ecf
Marlon Ercillo <mercillo729@gmail.com>
2016-07-08 01:13:54 -0700

    Tried to add reviews page

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
  $(document).ready(function(){
 
   $(".contentContainer").css("min-height", $(window).height());
-
+(function() { var s = document.createElement("script");
+	s.async = true;
+	s.onload = s.onreadystatechange = function(){
+		getYelpWidget("micks-karma-bar-irvine","300","RED","y","y","3");
+	};
+		s.src='http://chrisawren.com/widgets/yelp/yelpv2.js' ;
+		var x = document.getElementsByTagName('script')[0];
+		x.parentNode.insertBefore(s, x);})();
 });
